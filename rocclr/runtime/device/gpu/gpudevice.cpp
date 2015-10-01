fa313c6505b86c047a7c24fc38c680add5d520cb
foreman <dl.constructicon@amd.com>
2015-10-01 16:27:15 -0400

    P4 to Git Change 1196382 by rili@rili_opencl_stg on 2015/10/01 16:18:37
    	SWDEV-58155 -  Reduce maximum size of queue on device from 16MB to 8MB.
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/runtime/device/gpu/gpudevice.cpp#529 edit
    
    
    [ROCm/clr commit: 3847ee8ceb05127a5b7ae7b2774324652f44faeb]

--- rocclr/runtime/device/gpu/gpudevice.cpp
+++ rocclr/runtime/device/gpu/gpudevice.cpp
@@ -466,7 +466,7 @@ void NullDevice::fillDeviceInfo(
         info_.queueOnDeviceProperties_ =
             CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE | CL_QUEUE_PROFILING_ENABLE;
         info_.queueOnDevicePreferredSize_ = 256 * Ki;
-        info_.queueOnDeviceMaxSize_ = 16 * Mi;
+        info_.queueOnDeviceMaxSize_ = 8 * Mi;
         info_.maxOnDeviceQueues_ = 1;
         info_.maxOnDeviceEvents_ = settings().numDeviceEvents_;
         info_.globalVariablePreferredTotalSize_ = static_cast<size_t>(info_.globalMemSize_);
