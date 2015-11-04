8a82228927d5c4fe2198240ca96be2c16b54ff86
foreman <dl.constructicon@amd.com>
2015-11-04 17:14:47 -0500

    P4 to Git Change 1207590 by xcui@merged_opencl_jxcwin on 2015/11/04 16:20:09
    	SWDEV-80174 - added an env variable OCL_SET_SVM_SIZE to change the SVM size of the device, if it support SVM. This is part of implemenation to change SVM size based on system memory size and device memory size.
    	This is the changes in OpenCL  side.
    
    	code review:
    	http://ocltc.amd.com/reviews/r/8893/
    
    	precheckin:
    	http://ocltc.amd.com:8111/viewModification.html?modId=61653&personal=true&buildTypeId=&tab=vcsModificationBuilds&show_all_builds=true
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/runtime/device/gpu/gslbe/src/rt/GSLDevice.cpp#149 edit
    ... //depot/stg/opencl/drivers/opencl/runtime/utils/flags.hpp#246 edit

--- rocclr/runtime/utils/flags.hpp
+++ rocclr/runtime/utils/flags.hpp
@@ -170,6 +170,8 @@ release(int, AMD_GPU_FORCE_SINGLE_FP_DENORM, -1,                              \
         "Force denorm for single precision: -1 - don't force, 0 - disable, 1 - enable")                \
 debug(bool, OCL_FORCE_CPU_SVM, false,                                         \
         "force svm support for CPU")                                          \
+debug(uint, OCL_SET_SVM_SIZE, 4096,                                           \
+        "set SVM space size for discrete GPU")                                \
 debug(uint, OCL_SYSMEM_REQUIREMENT, 2,                                        \
         "Use flag to change the minimum requirement of system memory not to downgrade")        \
 debug(bool, GPU_ENABLE_HW_DEBUG, false,                                       \
