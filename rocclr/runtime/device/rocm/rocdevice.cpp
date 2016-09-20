6f183a75db104681fe650065b1238390a821eb9e
foreman <dl.constructicon@amd.com>
2016-09-20 03:20:15 -0400

    P4 to Git Change 1316270 by scharupa@scharupa_temp on 2016/09/20 03:14:55
    	SWDEV-96479 - changing tab to space as Victor pointed out
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/runtime/device/rocm/rocdevice.cpp#18 edit
    
    
    [ROCm/clr commit: c2b57553dcaf2c8cdb16ed55b565ea4674fe1f8d]

--- rocclr/runtime/device/rocm/rocdevice.cpp
+++ rocclr/runtime/device/rocm/rocdevice.cpp
@@ -803,7 +803,7 @@ Device::populateOCLDeviceConstants()
     if (agent_profile_ == HSA_PROFILE_FULL) { // full-profile = participating in coherent memory,
                                               // base-profile = NUMA based non-coherent memory
 	info_.hostUnifiedMemory_ = CL_TRUE;
-	}
+    }
     info_.memBaseAddrAlign_ = 8 * (flagIsDefault(MEMOBJ_BASE_ADDR_ALIGN) ?
         sizeof(cl_long16) : MEMOBJ_BASE_ADDR_ALIGN);
     info_.minDataTypeAlignSize_ = sizeof(cl_long16);
