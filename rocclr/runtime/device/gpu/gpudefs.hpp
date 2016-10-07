a3b3bf8855048869d9aa2629770be30d47b39d5e
foreman <dl.constructicon@amd.com>
2016-10-07 12:26:34 -0400

    P4 to Git Change 1323904 by asalmanp@asalmanp-opencl-stg on 2016/10/07 12:18:30
    	SWDEV-104662 - [CQE OCL][Lexa][QR]: ERROR: clBuildProgram(-11) on running clinfo; Faulty CL 1322829
    
    	Fixing a regression caused by CL#1322829. the core gfxipVersion for Lexa should be 800 and this is what it's used in ORCAHSALoaderContext::IsaSupportedByAgent @gpuprogram.cpp
    
    	ReviewBoardURL = http://ocltc.amd.com/reviews/r/11526/
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/runtime/device/gpu/gpudefs.hpp#143 edit
    
    
    [ROCm/clr commit: e8488126a834f962813139ab84d13334188e1b53]

--- rocclr/runtime/device/gpu/gpudefs.hpp
+++ rocclr/runtime/device/gpu/gpudefs.hpp
@@ -127,7 +127,7 @@ static const AMDDeviceInfo DeviceInfo[] = {
 /* CAL_TARGET_BAFFIN */     { ED_ATI_CAL_MACHINE_BAFFIN_ISA,    "Baffin",       "baffin",       4, 16, 1, 256, 64 * Ki, 32, 800 },
 /* CAL_TARGET_GREENLAND */  { ED_ATI_CAL_MACHINE_GREENLAND_ISA, "gfx900",       "gfx900",       4, 16, 1, 256, 64 * Ki, 32, 900 },
 /* CAL_TARGET_STONEY */     { ED_ATI_CAL_MACHINE_STONEY_ISA,    "Stoney",       "stoney",       4, 16, 1, 256, 64 * Ki, 32, 800 },
-/* CAL_TARGET_LEXA */       { ED_ATI_CAL_MACHINE_LEXA_ISA,      "gfx804",       "gfx804",       4, 16, 1, 256, 64 * Ki, 32, 804 },
+/* CAL_TARGET_LEXA */       { ED_ATI_CAL_MACHINE_LEXA_ISA,      "gfx804",       "gfx804",       4, 16, 1, 256, 64 * Ki, 32, 800 },
 };
 
 enum gfx_handle {
