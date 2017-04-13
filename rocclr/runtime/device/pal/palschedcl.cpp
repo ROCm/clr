8e63ab5f9826430e5197f648866eb26039aa689a
foreman <dl.constructicon@amd.com>
2017-04-13 17:41:32 -0400

    P4 to Git Change 1398310 by gandryey@gera-w8 on 2017/04/13 17:30:33
    	SWDEV-79445 - OCL generic changes and code clean-up
    	- Fix PAL initialization
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/runtime/device/pal/palschedcl.cpp#5 edit
    
    
    [ROCm/clr commit: 8f69b6b08205baebc8be94313f6903eb7df1e2c6]

--- rocclr/runtime/device/pal/palschedcl.cpp
+++ rocclr/runtime/device/pal/palschedcl.cpp
@@ -5,9 +5,12 @@ namespace pal {
 
 #define SCHEDULER_KERNEL(...) #__VA_ARGS__
 
-const char* SchedulerSourceCode = SCHEDULER_KERNEL(% s
-\n extern void __amd_scheduler(__global void*, __global void*, uint);
-\n __kernel void scheduler(__global void* queue, __global void* params, uint paramIdx) {
+const char* SchedulerSourceCode = SCHEDULER_KERNEL(
+%s
+\n
+extern void __amd_scheduler(__global void*, __global void*, uint);
+\n
+__kernel void scheduler(__global void* queue, __global void* params, uint paramIdx) {
   __amd_scheduler(queue, params, paramIdx);
 }
 \n);
