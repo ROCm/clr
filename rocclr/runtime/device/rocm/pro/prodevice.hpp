6d7fb53d4a7ca3e02685202ca4dbb220c107f9d7
foreman <dl.constructicon@amd.com>
2017-05-18 14:47:03 -0400

    P4 to Git Change 1411361 by gandryey@gera-lnx-rcf-lc on 2017/05/18 14:22:45
    	SWDEV-121873 - [ROCm CQE][Vega10][QR] Observed Aborted (core dumped) while running clinfo due to CL#1410373
    	- Fix device handle default initialization
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/runtime/device/rocm/pro/prodevice.hpp#2 edit

--- rocclr/runtime/device/rocm/pro/prodevice.hpp
+++ rocclr/runtime/device/rocm/pro/prodevice.hpp
@@ -23,7 +23,7 @@ public:
     : file_desc_(0)
     , major_ver_(0)
     , minor_ver_(0)
-    , cp_ver_(0)
+    , dev_handle_(nullptr)
     , alloc_ops_(nullptr) {}
   virtual ~ProDevice() override;
 
@@ -37,7 +37,6 @@ private:
   int32_t               file_desc_;   //!< File descriptor for the device
   uint32_t              major_ver_;   //!< Major driver version
   uint32_t              minor_ver_;   //!< Minor driver version
-  uint32_t              cp_ver_;      //!< CP ucode version
   amdgpu_device_handle  dev_handle_;  //!< AMD gpu device handle
   amdgpu_gpu_info       gpu_info_;    //!< GPU info structure
   amdgpu_heap_info      heap_info_;   //!< Information about memory
