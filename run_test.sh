e89767690bde55e23bcb689a48a3a1f088c51630
Evgeny <evgeny.shcherbakov@amd.com>
2018-12-23 23:52:05 -0600

    adding assertion to run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -6,6 +6,7 @@ fatal() {
 }
 
 if [ -z "$BUILD_DIR" ] ; then fatal "env var BUILD_DIR is not defined"; fi
+if [ -z "$HCC_HOME" ] ; then fatal "env var HCC_HOME is not defined"; fi
 
 cd $BUILD_DIR
 ./run.sh
