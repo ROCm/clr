fbe3612b5e86b6113b9e7a313e60c88258944ba8
Evgeny <evgeny.shcherbakov@amd.com>
2019-01-23 21:05:31 -0600

    adding include dir for hsa/hsa.h

--- build.sh
+++ build.sh
@@ -7,29 +7,29 @@ fatal() {
   exit 1
 }
 
-if [ -z "$ROCTRACER_ROOT" ]; then export ROCTRACER_ROOT=$PWD; fi
-if [ -z "$BUILD_DIR" ] ; then export BUILD_DIR=$PWD; fi
+if [ -z "$ROCTRACER_ROOT" ]; then ROCTRACER_ROOT=$PWD; fi
+if [ -z "$BUILD_DIR" ] ; then BUILD_DIR=$PWD; fi
 if [ -z "$HIP_PATH" ] ; then export HIP_PATH="$ROCM_PATH/hip"; fi
 if [ -z "$HCC_HOME" ] ; then export HCC_HOME="$ROCM_PATH/hcc"; fi
-if [ -z "$BUILD_TYPE" ] ; then export BUILD_TYPR="release"; fi
-if [ -z "$PACKAGE_ROOT" ] ; then export PACKAGE_ROOT=$ROCM_PATH; fi
-if [ -z "$PACKAGE_PREFIX" ] ; then export PACKAGE_PREFIX="$ROCM_PATH/$COMPONENT"; fi
+if [ -z "$BUILD_TYPE" ] ; then BUILD_TYPE="release"; fi
+if [ -z "$PACKAGE_ROOT" ] ; then PACKAGE_ROOT=$ROCM_PATH; fi
+if [ -z "$PACKAGE_PREFIX" ] ; then PACKAGE_PREFIX="$ROCM_PATH/$COMPONENT"; fi
 
+ROCTRACER_ROOT=$(cd $ROCTRACER_ROOT && echo $PWD)
 MAKE_OPTS="-j 8 -C $BUILD_DIR"
 
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR
+
 cmake \
     -DCMAKE_MODULE_PATH=$ROCTRACER_ROOT/cmake_modules \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_PREFIX_PATH="$PACKAGE_ROOT/hsa/include/hsa;$PACKAGE_ROOT/hsa/lib;$PACKAGE_ROOT/libhsakmt/lib" \
+    -DCMAKE_PREFIX_PATH="$PACKAGE_ROOT" \
     -DCMAKE_INSTALL_PREFIX=$PACKAGE_ROOT \
     -DCPACK_PACKAGING_INSTALL_PREFIX=$PACKAGE_PREFIX \
     -DCPACK_GENERATOR="DEB;RPM" \
     $ROCTRACER_ROOT
-popd
-
-make $MAKE_OPTS
+make
 make mytest
 make package
 
