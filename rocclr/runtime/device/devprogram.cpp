c0a8a64fb66b6353e7d8c19f169fa1dac224a1fb
foreman <dl.swbuild@amd.com>
2019-08-02 14:21:59 -0400

    P4 to Git Change 1977826 by kzhuravl@kzhuravl-spot-ocl on 2019/08/02 14:18:50
    	SWDEV-79445 - OCL generic changes and code clean-up
    
    	Fix non-comgr path
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/runtime/device/devprogram.cpp#55 edit

--- rocclr/runtime/device/devprogram.cpp
+++ rocclr/runtime/device/devprogram.cpp
@@ -1766,7 +1766,7 @@ bool Program::linkImplLC(amd::option::Options* options) {
   }
 
   if (device().settings().lcWavefrontSize64_) {
-    codegenOptions.push_back("-mwavefrontsize64");
+    codegenOptions.append("-mwavefrontsize64");
   }
 
   // Tokenize the options string into a vector of strings
