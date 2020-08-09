152a2dfb5a851af9f2af2e9bfccf4674b8e549a3
Jason Tang <jatang@amd.com>
2020-08-09 11:28:09 -0400

    SWDEV-247463 - Fix regression: ocltst segfaults
    Change-Id: Iadb55ba45d6c8ade0757fd970ac4c6cde1805de3

--- rocclr/device/rocm/rocvirtual.cpp
+++ rocclr/device/rocm/rocvirtual.cpp
@@ -763,7 +763,8 @@ bool VirtualGPU::create() {
   gpu_queue_ = roc_device_.acquireQueue(queue_size, cooperative_, cuMask_, priority_);
   if (!gpu_queue_) return false;
 
-  if (!initPool(dev().settings().kernargPoolSize_, (amd::IS_HIP) ? queue_size : 0)) {
+  if (!initPool(dev().settings().kernargPoolSize_,
+                (profiling_ || (amd::IS_HIP)) ? queue_size : 0)) {
     LogError("Couldn't allocate arguments/signals for the queue");
     return false;
   }
