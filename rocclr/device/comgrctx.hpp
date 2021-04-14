ee3d40908543328c837697b5e680bc55ecc0182b
Vladislav Sytchenko <Vladislav.Sytchenko@amd.com>
2021-04-14 13:24:43 -0400

    SWDEV-280473 - Fix warnings in combined LC/HSAIL build
    Enabling both LC and HSAIL will cause the DYN macro to be redefined.
    Rename it for each compiler to avoid name clashing.
    
    Change-Id: I607f022f37c4d05bef4e3a8070d19bd3659d7bc2
    
    
    [ROCm/clr commit: b771377665e75639a6c0cf410faaf45acdd98fd2]

--- rocclr/device/comgrctx.hpp
+++ rocclr/device/comgrctx.hpp
@@ -121,13 +121,13 @@ struct ComgrEntryPoints {
   t_amd_comgr_symbol_get_info           amd_comgr_symbol_get_info;
 };
 
-#ifdef COMGR_DYN_DLL 
-#define DYN(NAME) cep_.NAME
+#ifdef COMGR_DYN_DLL
+#define COMGR_DYN(NAME) cep_.NAME
 #define GET_COMGR_SYMBOL(NAME) cep_.NAME = \
   reinterpret_cast<t_##NAME>(Os::getSymbol(cep_.handle, #NAME)); \
   if (nullptr == cep_.NAME) { return false; }
 #else
-#define DYN(NAME) NAME
+#define COMGR_DYN(NAME) NAME
 #define GET_COMGR_SYMBOL(NAME)
 #endif
 
@@ -140,139 +140,139 @@ public:
   static bool IsReady() { return is_ready_; }
 
   static void get_version(size_t *major, size_t *minor) {
-    DYN(amd_comgr_get_version)(major, minor);
+    COMGR_DYN(amd_comgr_get_version)(major, minor);
   }
   static amd_comgr_status_t status_string(amd_comgr_status_t status, const char ** status_string) {
-    return DYN(amd_comgr_status_string)(status, status_string);
+    return COMGR_DYN(amd_comgr_status_string)(status, status_string);
   }
   static amd_comgr_status_t get_isa_count(size_t *count) {
-    return DYN(amd_comgr_get_isa_count)(count);
+    return COMGR_DYN(amd_comgr_get_isa_count)(count);
   }
   static amd_comgr_status_t get_isa_name(size_t index, const char **isa_name) {
-    return DYN(amd_comgr_get_isa_name)(index, isa_name);
+    return COMGR_DYN(amd_comgr_get_isa_name)(index, isa_name);
   }
   static amd_comgr_status_t get_isa_metadata(const char *isa_name, amd_comgr_metadata_node_t *metadata) {
-    return DYN(amd_comgr_get_isa_metadata)(isa_name, metadata);
+    return COMGR_DYN(amd_comgr_get_isa_metadata)(isa_name, metadata);
   }
   static amd_comgr_status_t create_data(amd_comgr_data_kind_t kind, amd_comgr_data_t *data) {
-    return DYN(amd_comgr_create_data)(kind, data);
+    return COMGR_DYN(amd_comgr_create_data)(kind, data);
   }
   static amd_comgr_status_t release_data(amd_comgr_data_t data) {
-    return DYN(amd_comgr_release_data)(data);
+    return COMGR_DYN(amd_comgr_release_data)(data);
   }
   static amd_comgr_status_t get_data_kind(amd_comgr_data_t data, amd_comgr_data_kind_t *kind) {
-    return DYN(amd_comgr_get_data_kind)(data, kind);
+    return COMGR_DYN(amd_comgr_get_data_kind)(data, kind);
   }
   static amd_comgr_status_t set_data(amd_comgr_data_t data, size_t size, const char* bytes) {
-    return DYN(amd_comgr_set_data)(data, size, bytes);
+    return COMGR_DYN(amd_comgr_set_data)(data, size, bytes);
   }
   static amd_comgr_status_t set_data_name(amd_comgr_data_t data, const char* name) {
-    return DYN(amd_comgr_set_data_name)(data, name);
+    return COMGR_DYN(amd_comgr_set_data_name)(data, name);
   }
   static amd_comgr_status_t get_data(amd_comgr_data_t data, size_t *size, char *bytes) {
-    return DYN(amd_comgr_get_data)(data, size, bytes);
+    return COMGR_DYN(amd_comgr_get_data)(data, size, bytes);
   }
   static amd_comgr_status_t get_data_name(amd_comgr_data_t data, size_t *size, char *name) {
-    return DYN(amd_comgr_get_data_name)(data, size, name);
+    return COMGR_DYN(amd_comgr_get_data_name)(data, size, name);
   }
   static amd_comgr_status_t get_data_isa_name(amd_comgr_data_t data, size_t *size, char *isa_name) {
-    return DYN(amd_comgr_get_data_isa_name)(data, size, isa_name);
+    return COMGR_DYN(amd_comgr_get_data_isa_name)(data, size, isa_name);
   }
   static amd_comgr_status_t get_data_metadata(amd_comgr_data_t data, amd_comgr_metadata_node_t *metadata) {
-    return DYN(amd_comgr_get_data_metadata)(data, metadata);
+    return COMGR_DYN(amd_comgr_get_data_metadata)(data, metadata);
   }
   static amd_comgr_status_t destroy_metadata(amd_comgr_metadata_node_t metadata) {
-    return DYN(amd_comgr_destroy_metadata)(metadata);
+    return COMGR_DYN(amd_comgr_destroy_metadata)(metadata);
   }
   static amd_comgr_status_t create_data_set(amd_comgr_data_set_t *data_set) {
-    return DYN(amd_comgr_create_data_set)(data_set);
+    return COMGR_DYN(amd_comgr_create_data_set)(data_set);
   }
   static amd_comgr_status_t destroy_data_set(amd_comgr_data_set_t data_set) {
-    return DYN(amd_comgr_destroy_data_set)(data_set);
+    return COMGR_DYN(amd_comgr_destroy_data_set)(data_set);
   }
   static amd_comgr_status_t data_set_add(amd_comgr_data_set_t data_set, amd_comgr_data_t data) {
-    return DYN(amd_comgr_data_set_add)(data_set, data);
+    return COMGR_DYN(amd_comgr_data_set_add)(data_set, data);
   }
   static amd_comgr_status_t data_set_remove(amd_comgr_data_set_t data_set, amd_comgr_data_kind_t data_kind) {
-    return DYN(amd_comgr_data_set_remove)(data_set, data_kind);
+    return COMGR_DYN(amd_comgr_data_set_remove)(data_set, data_kind);
   }
   static amd_comgr_status_t action_data_count(amd_comgr_data_set_t data_set, amd_comgr_data_kind_t data_kind, size_t *count) {
-    return DYN(amd_comgr_action_data_count)(data_set, data_kind, count);
+    return COMGR_DYN(amd_comgr_action_data_count)(data_set, data_kind, count);
   }
   static amd_comgr_status_t action_data_get_data(amd_comgr_data_set_t data_set, amd_comgr_data_kind_t data_kind, size_t index, amd_comgr_data_t *data) {
-    return DYN(amd_comgr_action_data_get_data)(data_set, data_kind, index, data);
+    return COMGR_DYN(amd_comgr_action_data_get_data)(data_set, data_kind, index, data);
   }
   static amd_comgr_status_t create_action_info(amd_comgr_action_info_t *action_info) {
-    return DYN(amd_comgr_create_action_info)(action_info);
+    return COMGR_DYN(amd_comgr_create_action_info)(action_info);
   }
   static amd_comgr_status_t destroy_action_info(amd_comgr_action_info_t action_info) {
-    return DYN(amd_comgr_destroy_action_info)(action_info);
+    return COMGR_DYN(amd_comgr_destroy_action_info)(action_info);
   }
   static amd_comgr_status_t action_info_set_isa_name(amd_comgr_action_info_t action_info, const char *isa_name) {
-    return DYN(amd_comgr_action_info_set_isa_name)(action_info, isa_name);
+    return COMGR_DYN(amd_comgr_action_info_set_isa_name)(action_info, isa_name);
   }
   static amd_comgr_status_t action_info_get_isa_name(amd_comgr_action_info_t action_info, size_t *size, char *isa_name) {
-    return DYN(amd_comgr_action_info_get_isa_name)(action_info, size, isa_name);
+    return COMGR_DYN(amd_comgr_action_info_get_isa_name)(action_info, size, isa_name);
   }
   static amd_comgr_status_t action_info_set_language(amd_comgr_action_info_t action_info, amd_comgr_language_t language) {
-    return DYN(amd_comgr_action_info_set_language)(action_info, language);
+    return COMGR_DYN(amd_comgr_action_info_set_language)(action_info, language);
   }
   static amd_comgr_status_t action_info_get_language(amd_comgr_action_info_t action_info, amd_comgr_language_t *language) {
-    return DYN(amd_comgr_action_info_get_language)(action_info, language);
+    return COMGR_DYN(amd_comgr_action_info_get_language)(action_info, language);
   }
   static amd_comgr_status_t action_info_set_option_list(amd_comgr_action_info_t action_info, const char *options[], size_t count) {
-    return DYN(amd_comgr_action_info_set_option_list)(action_info, options, count);
+    return COMGR_DYN(amd_comgr_action_info_set_option_list)(action_info, options, count);
   }
   static amd_comgr_status_t action_info_get_option_list_count(amd_comgr_action_info_t action_info, size_t *count) {
-    return DYN(amd_comgr_action_info_get_option_list_count)(action_info, count);
+    return COMGR_DYN(amd_comgr_action_info_get_option_list_count)(action_info, count);
   }
   static amd_comgr_status_t action_info_get_option_list_item(amd_comgr_action_info_t action_info, size_t index, size_t *size, char *option) {
-    return DYN(amd_comgr_action_info_get_option_list_item)(action_info, index, size, option);
+    return COMGR_DYN(amd_comgr_action_info_get_option_list_item)(action_info, index, size, option);
   }
   static amd_comgr_status_t action_info_set_working_directory_path(amd_comgr_action_info_t action_info, const char *path) {
-    return DYN(amd_comgr_action_info_set_working_directory_path)(action_info, path);
+    return COMGR_DYN(amd_comgr_action_info_set_working_directory_path)(action_info, path);
   }
   static amd_comgr_status_t action_info_get_working_directory_path(amd_comgr_action_info_t action_info, size_t *size, char *path) {
-    return DYN(amd_comgr_action_info_get_working_directory_path)(action_info, size, path);
+    return COMGR_DYN(amd_comgr_action_info_get_working_directory_path)(action_info, size, path);
   }
   static amd_comgr_status_t action_info_set_logging(amd_comgr_action_info_t action_info, bool logging) {
-    return DYN(amd_comgr_action_info_set_logging)(action_info, logging);
+    return COMGR_DYN(amd_comgr_action_info_set_logging)(action_info, logging);
   }
   static amd_comgr_status_t action_info_get_logging(amd_comgr_action_info_t action_info, bool *logging) {
-    return DYN(amd_comgr_action_info_get_logging)(action_info, logging);
+    return COMGR_DYN(amd_comgr_action_info_get_logging)(action_info, logging);
   }
   static amd_comgr_status_t do_action(amd_comgr_action_kind_t kind, amd_comgr_action_info_t info, amd_comgr_data_set_t input, amd_comgr_data_set_t result) {
-    return DYN(amd_comgr_do_action)(kind, info, input, result);
+    return COMGR_DYN(amd_comgr_do_action)(kind, info, input, result);
   }
   static amd_comgr_status_t get_metadata_kind(amd_comgr_metadata_node_t metadata, amd_comgr_metadata_kind_t *kind) {
-    return DYN(amd_comgr_get_metadata_kind)(metadata, kind);
+    return COMGR_DYN(amd_comgr_get_metadata_kind)(metadata, kind);
   }
   static amd_comgr_status_t get_metadata_string(amd_comgr_metadata_node_t metadata, size_t *size, char *string) {
-    return DYN(amd_comgr_get_metadata_string)(metadata, size, string);
+    return COMGR_DYN(amd_comgr_get_metadata_string)(metadata, size, string);
   }
   static amd_comgr_status_t get_metadata_map_size(amd_comgr_metadata_node_t metadata, size_t *size) {
-    return DYN(amd_comgr_get_metadata_map_size)(metadata, size);
+    return COMGR_DYN(amd_comgr_get_metadata_map_size)(metadata, size);
   }
   static amd_comgr_status_t iterate_map_metadata(amd_comgr_metadata_node_t metadata, amd_comgr_status_t(*callback)(amd_comgr_metadata_node_t key, amd_comgr_metadata_node_t value, void *user_data), void *user_data) {
-    return DYN(amd_comgr_iterate_map_metadata)(metadata, callback, user_data);
+    return COMGR_DYN(amd_comgr_iterate_map_metadata)(metadata, callback, user_data);
   }
   static amd_comgr_status_t metadata_lookup(amd_comgr_metadata_node_t metadata, const char *key, amd_comgr_metadata_node_t *value) {
-    return DYN(amd_comgr_metadata_lookup)(metadata, key, value);
+    return COMGR_DYN(amd_comgr_metadata_lookup)(metadata, key, value);
   }
   static amd_comgr_status_t get_metadata_list_size(amd_comgr_metadata_node_t metadata, size_t *size) {
-    return DYN(amd_comgr_get_metadata_list_size)(metadata, size);
+    return COMGR_DYN(amd_comgr_get_metadata_list_size)(metadata, size);
   }
   static amd_comgr_status_t index_list_metadata(amd_comgr_metadata_node_t metadata, size_t index, amd_comgr_metadata_node_t *value) {
-    return DYN(amd_comgr_index_list_metadata)(metadata, index, value);
+    return COMGR_DYN(amd_comgr_index_list_metadata)(metadata, index, value);
   }
   static amd_comgr_status_t iterate_symbols(amd_comgr_data_t data, amd_comgr_status_t(*callback)(amd_comgr_symbol_t symbol, void *user_data), void *user_data) {
-    return DYN(amd_comgr_iterate_symbols)(data, callback, user_data);
+    return COMGR_DYN(amd_comgr_iterate_symbols)(data, callback, user_data);
   }
   static amd_comgr_status_t symbol_lookup(amd_comgr_data_t data, const char *name, amd_comgr_symbol_t *symbol) {
-    return DYN(amd_comgr_symbol_lookup)(data, name, symbol);
+    return COMGR_DYN(amd_comgr_symbol_lookup)(data, name, symbol);
   }
   static amd_comgr_status_t symbol_get_info(amd_comgr_symbol_t symbol, amd_comgr_symbol_info_t attribute, void *value) {
-    return DYN(amd_comgr_symbol_get_info)(symbol, attribute, value);
+    return COMGR_DYN(amd_comgr_symbol_get_info)(symbol, attribute, value);
   }
 
 private:
