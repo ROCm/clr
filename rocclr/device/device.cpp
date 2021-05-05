b818301d53ab89a76954d4051aa893a6efe36e3f
Alex Xie <AlexBin.Xie@amd.com>
2021-05-05 15:23:02 -0400

    SWDEV-290306 - [LNX][Navi24][mainline]clinfo test failed on Navi24
    Add Navi 24 support
    
    Change-Id: I7343384cf6fb8c532321e57e202c196ef054f459

--- rocclr/device/device.cpp
+++ rocclr/device/device.cpp
@@ -185,6 +185,7 @@ std::pair<const Isa*, const Isa*> Isa::supportedIsas() {
     {"gfx1030",                "gfx1030",   true,  true,  false,              10, 3,  0,    NONE,   NONE, 2,    32,   1,    256,    64 * Ki, 32},
     {"gfx1031",                "gfx1031",   true,  true,  false,              10, 3,  1,    NONE,   NONE, 2,    32,   1,    256,    64 * Ki, 32},
     {"gfx1032",                "gfx1032",   true,  true,  false,              10, 3,  2,    NONE,   NONE, 2,    32,   1,    256,    64 * Ki, 32},
+    {"gfx1034",                "gfx1034",   true,  false,  false,             10, 3,  4,    NONE,   NONE, 2,    32,   1,    256,    64 * Ki, 32}
   };
   return std::make_pair(std::begin(supportedIsas_), std::end(supportedIsas_));
 }
