b6fdb2c69a5fef7993321be6d77b2b451895fc20
Christophe Paquot <christophe.paquot@amd.com>
2021-10-29 17:41:10 -0700

    SWDEV-307109 - Don't force Persistent for HIP
    With SAM on, don't force Persistent for allocations
    in HIP. This makes ROCCLR go down paths we don't want
    for HIP.
    
    Change-Id: If54cc16fa891d4cfdc761c6ab21ad707627e822a
    
    
    [ROCm/clr commit: 52435527680252097d096119663acad128ed0de0]

--- rocclr/device/pal/palmemory.cpp
+++ rocclr/device/pal/palmemory.cpp
@@ -109,7 +109,7 @@ bool Memory::create(Resource::MemoryType memType, Resource::CreateParams* params
     // Assume that allocations will be placed into visible heap when ReBar is enabled
     // Only enable this assumption for small size local buffers
     constexpr size_t kLargeAlloc = (1ull << 27);
-    if ((memType == Local) && desc().buffer_ && (size() < kLargeAlloc) && dev().info().largeBar_) {
+    if (!amd::IS_HIP && (memType == Local) && desc().buffer_ && (size() < kLargeAlloc) && dev().info().largeBar_) {
       memType = Persistent;
     }
     // Create a resource in PAL
