e383c5fca0367aea2fd302da0414263d213a7d3c
sdashmiz <shadi.dashmiz@amd.com>
2022-03-03 12:50:17 -0500

    SWDEV-315402: Null param check for pGraphNode
    Signed-off-by: sdashmiz <shadi.dashmiz@amd.com>
    Change-Id: Ie1082bb5e5ebdbcc6d38bb1349cb879c2e85a453

--- hipamd/src/hip_graph.cpp
+++ hipamd/src/hip_graph.cpp
@@ -1628,7 +1628,8 @@ hipError_t hipGraphAddEventWaitNode(hipGraphNode_t* pGraphNode, hipGraph_t graph
                                     const hipGraphNode_t* pDependencies, size_t numDependencies,
                                     hipEvent_t event) {
   HIP_INIT_API(hipGraphAddEventWaitNode, pGraphNode, graph, pDependencies, numDependencies, event);
-  if (graph == nullptr || (numDependencies > 0 && pDependencies == nullptr) || event == nullptr) {
+  if (pGraphNode == nullptr || graph == nullptr || (numDependencies > 0 && pDependencies == nullptr)
+      || event == nullptr) {
     HIP_RETURN(hipErrorInvalidValue);
   }
   *pGraphNode = new hipGraphEventWaitNode(event);
