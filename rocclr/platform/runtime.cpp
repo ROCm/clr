b5e57178019e4a26885f807259c444d18455f4ec
haoyuan2 <Hao.Yuan@amd.com>
2022-04-05 17:23:28 -0700

    SWDEV-328274 - Move DLLMain from VDI layer to HIP/OCL layers
    Change-Id: Idc84eb0db92d21a5ced8769fa1eae064b86c31b0
    
    
    [ROCm/clr commit: 1fbc01a8124643b7ce19952675cb1933fd786509]

--- rocclr/platform/runtime.cpp
+++ rocclr/platform/runtime.cpp
@@ -119,40 +119,4 @@ uint ReferenceCountedObject::release() {
   return newCount;
 }
 
-#ifdef _WIN32
-#ifdef DEBUG
-static int reportHook(int reportType, char* message, int* returnValue) {
-  if (returnValue) {
-    *returnValue = 1;
-  }
-  std::cerr << message;
-  ::exit(3);
-  return TRUE;
-}
-#endif  // DEBUG
-
-extern "C" BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved) {
-  switch (reason) {
-    case DLL_PROCESS_ATTACH:
-#ifdef DEBUG
-      if (!::getenv("AMD_OCL_ENABLE_MESSAGE_BOX")) {
-        _CrtSetReportHook(reportHook);
-        _set_error_mode(_OUT_TO_STDERR);
-      }
-#endif  // DEBUG
-      break;
-    case DLL_PROCESS_DETACH:
-      amd::shutDown();
-      break;
-    case DLL_THREAD_DETACH: {
-      amd::Thread* thread = amd::Thread::current();
-      delete thread;
-    } break;
-    default:
-      break;
-  }
-  return true;
-}
-#endif
-
 }  // namespace amd
