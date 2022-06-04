356e22f910c8aafb8bd1c05b45e52be5c7db5bb9
Sarbojit Sarkar <sarbojit.sarkar@amd.com>
2022-06-04 07:57:54 +0000

    SWDEV-325379 - Fix for remote copy crash
    Change-Id: I22152c0b3538cf7cfc80f82505bc255c01d98f7b

--- rocclr/platform/memory.hpp
+++ rocclr/platform/memory.hpp
@@ -377,6 +377,8 @@ class Memory : public amd::RuntimeObject {
   //!save the user data during memory allocation
   UserData& getUserData() { return userData_; }
 
+  //!find if memory object is Arena memory
+  virtual bool isArena() { return false; }
 };
 
 //! Buffers are a specialization of memory. Just a wrapper, really,
@@ -673,6 +675,7 @@ public:
   ArenaMemory(Context& context)
     : Buffer(context, 0, std::numeric_limits<size_t>::max(),
              reinterpret_cast<void*>(kArenaMemoryPtr)) {}
+  bool isArena() { return true; }
 };
 
 }  // namespace amd
