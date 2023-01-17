9acba8b4a19d16f1993d8353c3d690d4d1465adc
Ranjith Ramakrishnan <Ranjith.Ramakrishnan@amd.com>
2023-01-17 14:01:03 -0800

    Split roctracer to runtime and dev/devel package
    SWDEV-356024 - Development package name will have suffix dev or devel based on OS
    Devel package contents - Header files, name link of public library files, html files and roctracer manual file
    Runtime package contents - Versioned public library files, private library files and license file
    
    Change-Id: I8ced3eab5d8824a66be39b9e777368506516b155

--- roctracer-backward-compat.cmake
+++ roctracer-backward-compat.cmake
@@ -109,8 +109,8 @@ endfunction()
 
 #Use template header file and generater wrapper header files
 generate_wrapper_header()
-install(DIRECTORY ${ROCT_WRAPPER_INC_DIR} DESTINATION ${PROJECT_NAME} COMPONENT runtime)
+install(DIRECTORY ${ROCT_WRAPPER_INC_DIR} DESTINATION ${PROJECT_NAME} COMPONENT dev)
 create_library_symlink()
-install(DIRECTORY ${ROCT_WRAPPER_LIB_DIR} DESTINATION ${PROJECT_NAME} COMPONENT runtime)
+install(DIRECTORY ${ROCT_WRAPPER_LIB_DIR} DESTINATION ${PROJECT_NAME} COMPONENT dev)
 #install soft link to tool
 #install(DIRECTORY ${ROCT_WRAPPER_TOOL_DIR} DESTINATION ${PROJECT_NAME})
