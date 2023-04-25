e65b1c2c5a34907870c44d4fef8281de917f85a2
Satyanvesh Dittakavi <Satyanvesh.Dittakavi@amd.com>
2023-04-25 06:28:33 +0000

    SWDEV-395413 - Fix fp16 header errors with -Werror in hiprtc
    Change-Id: I7875b91758e826b439c67ac9f211a20f9b6133f0
    
    
    [ROCm/clr commit: a67f354cec4a6f47e09b5884f97f76e2951d9049]

--- hipamd/include/hip/amd_detail/amd_hip_fp16.h
+++ hipamd/include/hip/amd_detail/amd_hip_fp16.h
@@ -24,6 +24,18 @@ THE SOFTWARE.
 #ifndef HIP_INCLUDE_HIP_AMD_DETAIL_HIP_FP16_H
 #define HIP_INCLUDE_HIP_AMD_DETAIL_HIP_FP16_H
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-identifier"
+#pragma clang diagnostic ignored "-Wreserved-macro-identifier"
+#pragma clang diagnostic ignored "-Wc++98-compat"
+#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wfloat-conversion"
+#pragma clang diagnostic ignored "-Wdouble-promotion"
+#pragma clang diagnostic ignored "-Wnested-anon-types"
+#pragma clang diagnostic ignored "-Wgnu-anonymous-struct"
+#pragma clang diagnostic ignored "-Wfloat-equal"
+
 #if defined(__HIPCC_RTC__)
   #define __HOST_DEVICE__ __device__
 #else
@@ -1775,4 +1787,5 @@ THE SOFTWARE.
     #endif
 #endif // !defined(__clang__) && defined(__GNUC__)
 
+#pragma clang diagnostic pop
 #endif // HIP_INCLUDE_HIP_AMD_DETAIL_HIP_FP16_H
