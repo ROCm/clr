002169534c126d56e1751550812a5129dd955b0c
Ioannis Assiouras <Ioannis.Assiouras@amd.com>
2023-08-11 15:08:12 +0100

    SWDEV-416741 - Improve logging in find_package(hip)
    Change-Id: Iaaaa595c668e5a8e977c9d2d31beca481748f637
    
    
    [ROCm/clr commit: 5e51335ef6b49959b0afc947f5a3a152da45fc6c]

--- hipamd/hip-config-amd.cmake
+++ hipamd/hip-config-amd.cmake
@@ -92,9 +92,6 @@ set_target_properties(hip::amdhip64 PROPERTIES
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
 
-get_target_property(amdhip64_type hip::amdhip64 TYPE)
-message(STATUS "hip::amdhip64 is ${amdhip64_type}")
-
 if(NOT WIN32)
   set_target_properties(hip::device PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
@@ -131,7 +128,7 @@ if(HIP_CLANG_NUM_PARALLEL_JOBS GREATER 1)
     endif()
     hip_add_interface_link_flags(hip::device -parallel-jobs=${HIP_CLANG_NUM_PARALLEL_JOBS})
   else()
-    message("clang compiler doesn't support parallel jobs")
+    message(AUTHOR_WARNING "clang compiler doesn't support parallel jobs")
   endif()
 endif()
 
@@ -147,7 +144,7 @@ execute_process(
 
 if( CLANGRT_Error )
   if (HIP_CXX_COMPILER MATCHES ".*clang\\+\\+")
-    message( STATUS "${HIP_CXX_COMPILER}: CLANGRT compiler options not supported.")
+    message(AUTHOR_WARNING "${HIP_CXX_COMPILER}: CLANGRT compiler options not supported.")
   endif()
 else()
   # Add support for __fp16 and _Float16, explicitly link with compiler-rt
@@ -156,6 +153,6 @@ else()
     set_property(TARGET hip::host APPEND PROPERTY INTERFACE_LINK_LIBRARIES "${CLANGRT_BUILTINS}")
     set_property(TARGET hip::device APPEND PROPERTY INTERFACE_LINK_LIBRARIES "${CLANGRT_BUILTINS}")
   else()
-    message(STATUS "clangrt builtins lib not found: ${CLANGRT_BUILTINS_FETCH_EXIT_CODE}")
+    message(AUTHOR_WARNING "clangrt builtins lib not found: ${CLANGRT_BUILTINS_FETCH_EXIT_CODE}")
   endif() # CLANGRT_BUILTINS_FETCH_EXIT_CODE Check
 endif() # CLANGRT_Error Check
