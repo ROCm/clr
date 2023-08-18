c83d976d51da9798b400526345981e155b7d664a
Juan Manuel MARTINEZ CAAMAÑO <juamarti@amd.com>
2023-08-18 09:56:08 +0200

    Revert "SWDEV-410182: Do not force wave64 for geekbench"
    This patch did not consider the dicussions in SWDEV-270908
    
    > "we found that in GeekBench5, forcing Wave64 instead of the default
    > Wave32 compute policy yields big gains in every subtest except one"
    
    This reverts commit d6cd06e81f4e3e77a90fec1a878d52d79b5c55b9.
    
    Change-Id: Ice1728585b9d1b2c1b36a06cfa0b8c47cb2bfa49
    
    
    [ROCm/clr commit: 06dcaae6191573e41ed666adf085e4c891bb82ea]

--- rocclr/device/pal/palsettings.cpp
+++ rocclr/device/pal/palsettings.cpp
@@ -193,6 +193,8 @@ bool Settings::create(const Pal::DeviceProperties& palProp,
 
   enableXNACK_ = enableXNACK;
   hsailExplicitXnack_ = enableXNACK;
+  bool useWavefront64 = false;
+
   std::string appName = {};
   std::string appPathAndName = {};
   amd::Os::getAppPathAndFileName(appName, appPathAndName);
@@ -213,6 +215,14 @@ bool Settings::create(const Pal::DeviceProperties& palProp,
     case Pal::AsicRevision::Navi23:
     case Pal::AsicRevision::Navi22:
     case Pal::AsicRevision::Navi21:
+      // set wavefront 64 for Geekbench 5
+      {
+        if (appName == "Geekbench 5.exe" ||
+            appName == "geekbench_x86_64.exe" ||
+            appName == "geekbench5.exe") {
+          useWavefront64 = true;
+        }
+      }
     // Fall through for Navi1x ...
     case Pal::AsicRevision::Navi14:
     case Pal::AsicRevision::Navi12:
@@ -231,6 +241,9 @@ bool Settings::create(const Pal::DeviceProperties& palProp,
       }
       if (!flagIsDefault(GPU_ENABLE_WAVE32_MODE)) {
         enableWave32Mode_ = GPU_ENABLE_WAVE32_MODE;
+      }
+      if (useWavefront64) {
+        enableWave32Mode_ = 0;
       }
       lcWavefrontSize64_ = !enableWave32Mode_;
       if (palProp.gfxLevel == Pal::GfxIpLevel::GfxIp10_1) {
