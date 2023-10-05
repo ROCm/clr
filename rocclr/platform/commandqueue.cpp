5438b6362e6ffb0864327d4005cb47980f4fbddc
German Andryeyev <German.Andryeyev@amd.com>
2023-10-05 17:33:31 -0400

    SWDEV-424603 - Force CPU wait if profiling
    Some pytorch tests use a tracer plugin and rely on profiling information
    to be reported right after hipDeviceSynchronize()
    
    Change-Id: Ib021a1e7b1a30b3c24de72627c471810f7f7878d

--- rocclr/platform/commandqueue.cpp
+++ rocclr/platform/commandqueue.cpp
@@ -124,6 +124,9 @@ void HostQueue::finish(bool cpu_wait) {
   // If command doesn't contain HW event and runtime didn't request CPU wait,
   // then force marker submit
   bool force_marker = false;
+  // Force CPU wait if profiler is enabled. Pytorch tests may use tracer's plugin and rely on
+  // profiling information to be available right after finish.
+  cpu_wait = activity_prof::IsEnabled(OP_ID_DISPATCH);
   if (AMD_DIRECT_DISPATCH && (command != nullptr) && !cpu_wait) {
     void* hw_event =
       (command->NotifyEvent() != nullptr) ? command->NotifyEvent()->HwEvent() : command->HwEvent();
