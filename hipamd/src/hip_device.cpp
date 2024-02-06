3e575a17d9ba9f1d05c12aca288fe74fa68f253a
Satyanvesh Dittakavi <Satyanvesh.Dittakavi@amd.com>
2024-02-06 08:09:21 +0000

    SWDEV-400448 - SWDEV-392872 - Optimize to add lock only if its null stream
    Change-Id: Ia905109a295614e8ce4e2bd24261c86ba8138178
    
    
    [ROCm/clr commit: a6841e3f4309bdb271ff9775d5f9eb2e7c87a68d]

--- hipamd/src/hip_device.cpp
+++ hipamd/src/hip_device.cpp
@@ -31,11 +31,11 @@ namespace hip {
 
 // ================================================================================================
 hip::Stream* Device::NullStream(bool wait) {
-  {
-     amd::ScopedLock lock(lock_);
-     if (null_stream_ == nullptr) {
-       null_stream_ = new Stream(this, Stream::Priority::Normal, 0, true);
-     }
+  if (null_stream_ == nullptr) {
+    amd::ScopedLock lock(lock_);
+    if (null_stream_ == nullptr) {
+      null_stream_ = new Stream(this, Stream::Priority::Normal, 0, true);
+    }
   }
   if (null_stream_ == nullptr) {
     return nullptr;
