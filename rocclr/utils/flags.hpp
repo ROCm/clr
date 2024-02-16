c6a01a2cbaf0f7a3f3599f91cc3622c2ff5645f7
Todd tiantuo Li <Toddtiantuo.Li@amd.com>
2024-02-16 12:33:39 -0800

    SWDEV-333557 - Enable PAL_HIP_IPC_FLAG by default
    Change-Id: Ibb2ca0b9521aff4eca190e4817dcc5f8d697b172
    
    
    [ROCm/clr commit: 7bfee3481ba0196394c26cdd3e8ea7b6e1b2baa5]

--- rocclr/utils/flags.hpp
+++ rocclr/utils/flags.hpp
@@ -198,7 +198,7 @@ release(bool, HIP_MEM_POOL_SUPPORT, false,                                    \
         "Enables memory pool support in HIP")                                 \
 release(bool, HIP_MEM_POOL_USE_VM, IS_WINDOWS,                                \
         "Enables memory pool support in HIP")                                 \
-release(bool, PAL_HIP_IPC_FLAG, false,                                        \
+release(bool, PAL_HIP_IPC_FLAG, true,                                        \
         "Enable interprocess flag for device allocation in PAL HIP")          \
 release(uint, PAL_FORCE_ASIC_REVISION, 0,                                     \
         "Force a specific asic revision for all devices")                     \
