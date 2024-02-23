b434754d8557ec1902d5c7c202db927c35dfe620
Satyanvesh Dittakavi <Satyanvesh.Dittakavi@amd.com>
2024-02-23 11:48:16 +0000

    SWDEV-446781 - Check validity of hiprtc link state before using it
    Change-Id: I864e8d1a79bb28d9df0e4ee528cb50a96bc245cc
    
    
    [ROCm/clr commit: 15a058cb6036bf9ca5142fbb1d4cd7f79b23fc02]

--- hipamd/src/hiprtc/hiprtcInternal.hpp
+++ hipamd/src/hiprtc/hiprtcInternal.hpp
@@ -271,13 +271,18 @@ class RTCLinkProgram : public RTCProgram {
   // Private Data Members
   amd_comgr_data_set_t link_input_;
   std::vector<std::string> link_options_;
+  static std::unordered_set<RTCLinkProgram*> linker_set_;
 
   bool AddLinkerDataImpl(std::vector<char>& link_data, hiprtcJITInputType input_type,
                          std::string& link_file_name);
 
  public:
   RTCLinkProgram(std::string name);
-  ~RTCLinkProgram() { amd::Comgr::destroy_data_set(link_input_); }
+  ~RTCLinkProgram() {
+    amd::ScopedLock lock(lock_);
+    linker_set_.erase(this);
+    amd::Comgr::destroy_data_set(link_input_);
+  }
   // Public Member Functions
   bool AddLinkerOptions(unsigned int num_options, hiprtcJIT_option* options_ptr,
                         void** options_vals_ptr);
@@ -286,6 +291,7 @@ class RTCLinkProgram : public RTCProgram {
                      hiprtcJITInputType input_type);
   bool LinkComplete(void** bin_out, size_t* size_out);
   void AppendLinkerOptions() { AppendOptions(HIPRTC_LINK_OPTIONS_APPEND, &link_options_); }
+  static bool isLinkerValid(RTCLinkProgram* link_program);
 };
 
 // Thread Local Storage Variables Aggregator Class
