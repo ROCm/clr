1d48f2a1ab38b632919c4b7274899b3faf4279ff
Jaydeep Patel <jaydeepkumar.patel@amd.com>
2024-04-12 08:58:46 +0000

    SWDEV-456279 - Adding new hip flag to access contiguous memory and pass the flag to HSA API.
    Change-Id: I1bafeaa3096395c729723af958d609bc41e7845c

--- hipamd/src/hip_memory.cpp
+++ hipamd/src/hip_memory.cpp
@@ -589,6 +589,8 @@ hipError_t hipExtMallocWithFlags(void** ptr, size_t sizeBytes, unsigned int flag
     ihipFlags = CL_MEM_SVM_ATOMICS;
   } else if (flags == hipDeviceMallocUncached) {
     ihipFlags = CL_MEM_SVM_ATOMICS | ROCCLR_MEM_HSA_UNCACHED;
+  } else if (flags == hipDeviceMallocContiguous) {
+    ihipFlags = ROCCLR_MEM_HSA_CONTIGUOUS | ROCCLR_MEM_HSA_UNCACHED;
   } else if (flags == hipMallocSignalMemory) {
     ihipFlags = CL_MEM_SVM_ATOMICS | CL_MEM_SVM_FINE_GRAIN_BUFFER | ROCCLR_MEM_HSA_SIGNAL_MEMORY;
     if (sizeBytes != 8) {
