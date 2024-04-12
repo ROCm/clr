19c5c50c0109be43c6d9d1541318f5cf88380d31
German Andryeyev <German.Andryeyev@amd.com>
2024-04-12 11:22:15 -0400

    SWDEV-444670 - Enable teardown class
    Force implicit runtime teardown with a global destructor.
    
    Change-Id: Iabe63dedf5b94fefc98668585c45a61607120669
    
    
    [ROCm/clr commit: c95a75a2bff92d1a58e91ebb77fe80ae7b3fa9a0]

--- hipamd/src/hip_context.cpp
+++ hipamd/src/hip_context.cpp
@@ -61,6 +61,7 @@ void init(bool* status) {
       return;
     }
     g_devices.push_back(device);
+    amd::RuntimeTearDown::RegisterObject(device);
   }
 
   amd::Context* hContext = new amd::Context(devices, amd::Context::Info());
@@ -73,6 +74,7 @@ void init(bool* status) {
     hContext->release();
   }
   host_context = hContext;
+  amd::RuntimeTearDown::RegisterObject(hContext);
 
   PlatformState::instance().init();
   *status = true;
