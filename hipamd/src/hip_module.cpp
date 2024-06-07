9c03f85f46847be1ca1d2ee0dd2caee83f9d62f4
kjayapra-amd <karthik.jayaprakash@amd.com>
2024-06-07 11:00:04 -0400

    SWDEV-460948 - Remove dflock, since kernel arguments are part of command now.
    Change-Id: I6b5a229307b41bd24ffa0bc172c64ad1154df474

--- hipamd/src/hip_module.cpp
+++ hipamd/src/hip_module.cpp
@@ -371,7 +371,8 @@ hipError_t ihipModuleLaunchKernel(hipFunction_t f, uint32_t globalWorkSizeX,
   }
   hip::DeviceFunc* function = hip::DeviceFunc::asFunction(f);
   amd::Kernel* kernel = function->kernel();
-  amd::ScopedLock lock(function->dflock_);
+
+  amd::ScopedLock lock (DEBUG_HIP_KERNARG_COPY_OPT ? &function->dflock_ : nullptr); 
 
   hipError_t status = ihipLaunchKernel_validate(
       f, globalWorkSizeX, globalWorkSizeY, globalWorkSizeZ, blockDimX, blockDimY, blockDimZ,
