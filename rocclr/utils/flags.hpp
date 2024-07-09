f8598dabb04127c57f4501685e98b04587cc116d
taosang2 <tao.sang@amd.com>
2024-07-09 10:31:12 -0400

    SWDEV-458943 - make new AMD_MONITOR on
    make DEBUG_CLR_USE_STDMUTEX_IN_AMD_MONITOR be true
    
    Change-Id: I1d21378ff462478d3238d71e4e2a1a7d6b9167ac

--- rocclr/utils/flags.hpp
+++ rocclr/utils/flags.hpp
@@ -253,7 +253,7 @@ release(bool, HIP_ALWAYS_USE_NEW_COMGR_UNBUNDLING_ACTION, false,              \
         "Force to always use new comgr unbundling action")                    \
 release(bool, DEBUG_HIP_KERNARG_COPY_OPT, true,                               \
         "Enable/Disable multiple kern arg copies")                            \
-release(bool, DEBUG_CLR_USE_STDMUTEX_IN_AMD_MONITOR, false,                   \
+release(bool, DEBUG_CLR_USE_STDMUTEX_IN_AMD_MONITOR, true,                    \
         "Use std::mutext in amd::monotor")                                    \
 
 namespace amd {
