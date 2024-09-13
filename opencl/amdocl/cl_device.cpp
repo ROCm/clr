73d2e31b29d833ec6a702f9d3b88eb27198b8ee5
Julia Jiang <julia.jiang@amd.com>
2024-09-13 15:10:14 -0400

    SWDEV-412099 - Fix CTS clFillImage sub-tests failures
    Change-Id: I082476837c539e6ccf93cba6b1e97aae2509e65c
    
    
    [ROCm/clr commit: 17c8b9f8555513afd06c60e0dd66a905addef1ad]

--- opencl/amdocl/cl_device.cpp
+++ opencl/amdocl/cl_device.cpp
@@ -446,6 +446,14 @@ RUNTIME_ENTRY(cl_int, clGetDeviceInfo,
         return CL_SUCCESS;
       }
       CASE(CL_DEVICE_PCIE_ID_AMD, pcieDeviceId_);
+#define CL_DEVICE_NUMERIC_VERSION 0x105E
+      case CL_DEVICE_NUMERIC_VERSION: {
+        std::string driverVersion = as_amd(device)->info().driverVersion_;
+        std::string drvVersion(driverVersion.c_str());
+        drvVersion = drvVersion.substr(0, 4);  // calVersion.find_first_of("(") + 4);
+        cl_uint numVersion = atoi(drvVersion.c_str());
+        return amd::clGetInfo(numVersion, param_value_size, param_value, param_value_size_ret);
+      }
       default:
         break;
     }
