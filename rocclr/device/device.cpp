af1fd63823e26628670d434f055bf3cd445a8071
Stella Laurenzo <stellaraccident@gmail.com>
2025-05-15 16:39:40 -0400

    Use is_versioned = true consistently in both Comgr::LoadLib paths.

--- rocclr/device/device.cpp
+++ rocclr/device/device.cpp
@@ -758,7 +758,7 @@ bool Device::ValidateComgr() {
 #if defined(USE_COMGR_LIBRARY)
   // Check if Lightning compiler was requested
   if (settings_->useLightning_) {
-    constexpr bool kComgrVersioned = false;
+    constexpr bool kComgrVersioned = true;
     std::call_once(amd::Comgr::initialized, amd::Comgr::LoadLib, kComgrVersioned);
     // Use Lightning only if it's available
     settings_->useLightning_ = amd::Comgr::IsReady();
