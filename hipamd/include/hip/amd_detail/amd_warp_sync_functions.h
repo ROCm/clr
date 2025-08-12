7d6f6030c7bab8abd87a44fb528269cfab735395
Hashem Hashemi <hashem.hashemi@amd.com>
2025-08-12 22:25:48 +0000

    correction2

--- hipamd/include/hip/amd_detail/amd_warp_sync_functions.h
+++ hipamd/include/hip/amd_detail/amd_warp_sync_functions.h
@@ -242,8 +242,8 @@ unsigned long long __match_any(T value) {
     union dill { unsigned int i[2]; unsigned long long ill; decltype(value) val; } dill_ = { .val = value };
     retval = 1;
     //Do a full rotate of the wave lanes, using dpp with "wave_rol1" control (ID: 0x134).
-    //wave_rol1 dpp rotates the value from each lane to one lane right across the whole wave.
-    //In doing so each lane gets a mask of matches with all other lanes in the wave.
+    //wave_rol1 dpp rotates the value from each lane to one lane left of it, across the whole wave.
+    //In doing so each lane gets a mask of matches with all other lanes in the wave in retval.
     for (int i = 1; i < static_cast<int>(warpSize); i++) {
       if constexpr (sizeof(value) == 8)
         dill_.ill = __builtin_amdgcn_mov_dpp(dill_.ill, 0x134 /*dpp_ctrl=wave_rol1*/, 0xf/*row_mask*/, 0xf/*clmn_mask*/, 1/*bound_ctrl*/);
