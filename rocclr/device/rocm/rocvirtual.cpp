57148f9093e68bd455b6e3d7feb4af207ca1a7fc
Min Zhou <zhoumin@loongson.cn>
2025-08-19 20:03:00 +0800

    Fix allocation size for Device::MGSyncInfo object
    Signed-off-by: Min Zhou <zhoumin@loongson.cn>

--- rocclr/device/rocm/rocvirtual.cpp
+++ rocclr/device/rocm/rocvirtual.cpp
@@ -3416,7 +3416,7 @@ bool VirtualGPU::submitKernelInternal(const amd::NDRangeContainer& sizes,
           syncInfo->mgs = reinterpret_cast<Device::MGSyncData*>(dev().MGSync() +
                           Device::kMGInfoSizePerDevice * vcmd->firstDevice());
         } else if (singleGridSync) {
-          syncInfo = reinterpret_cast<Device::MGSyncInfo*>(allocKernArg(Device::kSGInfoSize, 64));
+          syncInfo = reinterpret_cast<Device::MGSyncInfo*>(allocKernArg(sizeof(Device::MGSyncInfo), 64));
           syncInfo->mgs = nullptr;
         }
         if (multiGridSync || singleGridSync) {
