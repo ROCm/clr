0c33dfa05b54561e378a5a0fd6e529c2580b7458
Ethan Trinh <Ethan.Trinh@amd.com>
2026-03-06 16:03:50 +0000

    Update copyright to use SPDX IDs [rocm-systems] ROCm/rocm-systems#3805 (commit 97a5dd9)

--- hipamd/include/hip/amd_detail/grid_launch.h
+++ hipamd/include/hip/amd_detail/grid_launch.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+ *
+ * SPDX-License-Identifier: MIT
+ */
 #pragma once
 
 #include <stdint.h>
--- hipamd/include/hip/amd_detail/hip_fp16_gcc.h
+++ hipamd/include/hip/amd_detail/hip_fp16_gcc.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+ *
+ * SPDX-License-Identifier: MIT
+ */
 #pragma once
 
 #if defined(__cplusplus)
