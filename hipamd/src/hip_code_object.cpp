82c6b3683a35d51eb202dc0ec158c0c49ccc6149
apophis <23428582+Apophis3158@users.noreply.github.com>
2026-06-08 18:49:00 +0000

    downgrade DynFunc error log to info [rocm-systems] ROCm/rocm-systems#6623 (commit 30e0163)

--- hipamd/src/hip_code_object.cpp
+++ hipamd/src/hip_code_object.cpp
@@ -107,7 +107,7 @@ hipError_t DynCO::getDynFunc(hipFunction_t* hfunc, const std::string& func_name)
 
   auto it = functions_.find(func_name);
   if (it == functions_.end()) {
-    LogPrintfError("Cannot find the function: %s ", func_name.c_str());
+    LogPrintfInfo("Cannot find the function: %s", func_name.c_str());
     return hipErrorNotFound;
   }
 
--- hipamd/src/hip_module.cpp
+++ hipamd/src/hip_module.cpp
@@ -74,7 +74,7 @@ hipError_t hipModuleGetFunction(hipFunction_t* hfunc, hipModule_t hmod, const ch
   }
 
   if (hipSuccess != PlatformState::Instance().GetDynFunc(hfunc, hmod, name)) {
-    LogPrintfError("Cannot find the function: %s for module: 0x%x", name, hmod);
+    LogPrintfInfo("Cannot find the function: %s for module: 0x%x", name, hmod);
     hip::tls.last_command_error_ = hipErrorNotFound;
     return hipErrorNotFound;
   }
