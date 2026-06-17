da5cb8f696c28bcdccc9c3d02658b618c951ae04
Karthik Jayaprakash <54370791+kjayapra-amd@users.noreply.github.com>
2026-06-17 19:19:45 +0000

    Enabling fast path fp8 support for gfx1250.
    ## Motivation
    
    Enabling fast path fp8 support for gfx1250.
    
    ## Technical Details
    
    Enabling fast path fp8 support for gfx1250.
    
    ## JIRA ID
    
    ROCM-25847
    
    ## Test Plan
    
    CI Tests
    
    ## Test Result
    
    CI Test Results
    
    ## Submission Checklist
    
    - [X ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    Co-authored-by: chiranjeevi pattigidi <cpattigi@amd.com>
    [rocm-systems] ROCm/rocm-systems#7117 (commit 020d0c3)

--- hipamd/include/hip/amd_detail/amd_hip_fp8.h
+++ hipamd/include/hip/amd_detail/amd_hip_fp8.h
@@ -13,7 +13,7 @@
 #define _HIP_INCLUDE_HIP_AMD_DETAIL_HIP_FP8_H_
 
 #if (defined(__gfx942__) || defined(__gfx1200__) || defined(__gfx1201__) ||                        \
-     defined(__gfx950__)) &&                                                                       \
+     defined(__gfx950__) || defined (__gfx1250__)) &&                                              \
     __HIP_DEVICE_COMPILE__
 #define HIP_FP8_CVT_FAST_PATH 1
 #else
@@ -23,8 +23,9 @@
 #if defined(__gfx942__) && __HIP_DEVICE_COMPILE__
 #define HIP_FP8_TYPE_OCP 0
 #define HIP_FP8_TYPE_FNUZ 1
-#elif (defined(__gfx1200__) || defined(__gfx1201__) || defined(__gfx950__)) &&                     \
-    __HIP_DEVICE_COMPILE__
+#elif (defined(__gfx1200__) || defined(__gfx1201__) || defined(__gfx950__) ||                      \
+       defined(__gfx1250__)) &&                                                                    \
+       __HIP_DEVICE_COMPILE__
 #define HIP_FP8_TYPE_OCP 1
 #define HIP_FP8_TYPE_FNUZ 0
 #else
