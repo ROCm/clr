1d2bf1c7e39b878cde11edb19bee76776ec1a2d3
Julia Jiang <56359287+jujiang-del@users.noreply.github.com>
2026-07-22 15:17:43 +0000

    fix(clr): Adding one more condition for VMM vadalition  (#8881)
    ## Motivation
    
    To fix the hip sub-test failure
    Unit_hipGraphMemFreeNodeGetParams_ValidArgs
    
    ## Technical Details
    
    JIRA ID: ROCM-27686, Add one more condition for validation for VMM.
    Besides, fix a build error on Ubuntu 22.04 caused by combination of
    compiler flags.
    
    ## Issue Tracking
    
    ROCM-27686
    
    ## Test Plan
    
    Run hip catch unit sub-test: ctest -R
    Unit_hipGraphMemFreeNodeGetParams_ValidArgs
    
    ## Test Result
    
    Should pass
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/rocm-systems/blob/develop/CONTRIBUTING.md.
    [rocm-systems] ROCm/rocm-systems#8881 (commit 1493e74)

--- hipamd/src/hip_mempool_impl.hpp
+++ hipamd/src/hip_mempool_impl.hpp
@@ -225,7 +225,7 @@ class MemoryPool : public amd::ReferenceCountedObject, amd::VmHeapArray {
     }
     state_.interprocess_ = properties_.handleTypes != hipMemHandleTypeNone;
     // Check if VM heap can be enabled
-    if (DEBUG_HIP_MEM_POOL_VMHEAP && AMD_DIRECT_DISPATCH &&
+    if (DEBUG_HIP_MEM_POOL_VMHEAP && AMD_DIRECT_DISPATCH && HIP_MEM_POOL_USE_VM &&
         !state_.phys_mem_ && !state_.interprocess_) {
       state_.use_vm_heap_ = true;
       busy_heap_.EnableVmHeap();
